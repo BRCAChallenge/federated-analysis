7d8bb16a94099fe3dcda3f6375301858507a9870
James Casaletto <james.casaletto@gmail.com>
2021-11-01 07:37:13 -0700

    make all paths absolute in wdl runme

--- wdl/runMe.sh
+++ wdl/runMe.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 
-DATA_DIR=/private/groups/patenlab/jcasalet/TOPMED_WORKDIR/DATA/F8/GT40/13
+ANNO_FILE=/private/groups/patenlab/jcasalet/TOPMED_WORKDIR/DATA/F8/GT40/13/freeze8_sample_annot_2020-07-07.txt
 CHROM=13
 GENE=BRCA2
 P2=0.001
-VCF_FILENAME=f8_chr13_brca2_gruhmb_age.vcf
+VCF_FILENAME=/private/groups/patenlab/jcasalet/TOPMED_WORKDIR/DATA/F8/GT40/13/f8_chr13_brca2_gruhmb_age.vcf
 SAVE_FILES=True
 GNOMAD_FILE=/private/groups/patenlab/jcasalet/federated-analysis/data/gnomad_genome_31_sites_chr13_brca2.vcf
-
-TOP_DIR=/private/groups/patenlab/jcasalet/federated-analysis
+VARIANT_PATHOGENICITY_FILE=/private/groups/patenlab/jcasalet/federated-analysis/data/brca-variants.tsv
 PYTHON_SCRIPT=${TOP_DIR}/app/cooccurrence/cooccurrenceFinder.py
 
 miniwdl cromwell /private/groups/patenlab/jcasalet/WDL/myVusCooccur.wdl \
 PYTHON_SCRIPT=${PYTHON_SCRIPT} \
-VCF_FILE=${DATA_DIR}/$VCF_FILENAME \
-ANNO_FILE=${DATA_DIR}/freeze8_sample_annot_2020-07-07.txt \
-VARIANT_PATHOGENICITY_FILE=${TOP_DIR}/data/brca-variants.tsv \
+VCF_FILE=$VCF_FILENAME \
+ANNO_FILE=${ANNO_FILE} \
+VARIANT_PATHOGENICITY_FILE=$VARIANT_PATHOGENICITY_FILE \
 OUTPUT_FILENAME=${GENE}-cooccurrences.json \
 ALL_FILENAME=${GENE}-all.json \
 VPI_FILENAME=${GENE}-vpi.json \
