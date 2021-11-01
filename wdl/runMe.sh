6452306263ea1bfddfcee6fdf6bf0a273146a874
James Casaletto <james.casaletto@gmail.com>
2021-11-01 07:43:47 -0700

    all paths absolute in wdl runme

--- wdl/runMe.sh
+++ wdl/runMe.sh
@@ -8,8 +8,9 @@ P2=0.001
 VCF_FILENAME=/private/groups/patenlab/jcasalet/TOPMED_WORKDIR/DATA/F8/GT40/13/f8_chr13_brca2_gruhmb_age.vcf
 SAVE_FILES=True
 GNOMAD_FILE=/private/groups/patenlab/jcasalet/federated-analysis/data/gnomad_genome_31_sites_chr13_brca2.vcf
-VARIANT_PATHOGENICITY_FILE=/private/groups/patenlab/jcasalet/federated-analysis/data/brca-variants.tsv
-PYTHON_SCRIPT=${TOP_DIR}/app/cooccurrence/cooccurrenceFinder.py
+VARIANT_PATHOGENICITY_FILE=/private/groups/patenlab/jcasalet/federated-analysis/data/clinvar.vcf
+PYTHON_SCRIPT=/private/groups/patenlab/jcasalet/federated-analysis/app/cooccurrence/cooccurrenceFinder.py
+PATHOLOGY_FILE=/private/groups/patenlab/jcasalet/federated-analysis/examples/BRCA2/data/brca2-pathology.tsv
 
 miniwdl cromwell /private/groups/patenlab/jcasalet/WDL/myVusCooccur.wdl \
 PYTHON_SCRIPT=${PYTHON_SCRIPT} \
@@ -30,4 +31,5 @@ GENE=$GENE \
 NUM_CORES=$(grep -c processor /proc/cpuinfo) \
 SAVE_FILES=$SAVE_FILES \
 GNOMAD_FILE=$GNOMAD_FILE \
+PATHOLOGY_FILE=$PATHOLOGY_FILE \
 -c /private/groups/patenlab/jcasalet/WDL/cromwell.local.conf 
